7b64002ae9f071b997a56092f784bc84c5174caa
Michael S. Downs <michael.s.downs@gmail.com>
2024-01-18 15:48:39 -0500

    adding assets folder

--- LangchainQueryChinookSQLiteDB-SQLandData.py
+++ LangchainQueryChinookSQLiteDB-SQLandData.py
@@ -1,14 +1,11 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.utilities import SQLDatabase
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.agents.agent_types import AgentType
 from langchain.chains.sql_database.query import create_sql_query_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.utilities import SQLDatabase
+import pandas as pd
 
 # Load environment variables from .env file (Optional)
 load_dotenv()
@@ -20,20 +17,23 @@ DB_ABS_PATH=os.getenv("DB_ABS_PATH")
 db = SQLDatabase.from_uri("sqlite:///{DB_ABS_PATH}")
 chain = create_sql_query_chain(llm=ChatOpenAI(temperature=0,model="gpt-3.5-turbo-0613"), db=db)
 
+wrongSqlQuery = "Select * From Album"
+
 def main():
     # Set the title and subtitle of the app
-    st.title("🦜🔗 Talk to a SQL DB with Natural Language")
+    st.title("🦜🔗 Talk to a SQL DB using LLMs")
     st.image('.././assets/sqliteERD.png')
-    st.subheader('This example returns JUST the SQL from your natural language prompt.')
+    st.subheader('This example returns BOTH the SQL AND its execution.')
     question = st.text_input("Ask a question (query/prompt)")
     if st.button("Submit Query", type="primary"):
 
         sql_query = chain.invoke({"question": question})
-        st.subheader("This is the SQL:")
+        st.subheader("This is the SQL you asked for:")
         st.write(sql_query)
-       #st.subheader("This is the Data from executing the SQL:")
-        #/df = db.run(sql_query)
-        #st.dataframe(df)
+        st.subheader("This is the Data from running that SQL:")
+        result = db.run(sql_query)
+        df = pd.DataFrame([dict(row) for row in result])
+        st.dataframe(df)
 
 if __name__ == '__main__':
     main()
