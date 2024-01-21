a0ed4c863c7e416d7a4db1a32b42c9d0cd44fe4e
Michael S. Downs <michael.s.downs@gmail.com>
2024-01-21 14:27:03 -0500

    last commits before moving to Coca-Cola VDI

--- LangchainQueryChinookSQLiteDB-SQLandData.py
+++ LangchainQueryChinookSQLiteDB-SQLandData.py
@@ -4,17 +4,20 @@ from dotenv import load_dotenv
 from langchain.utilities import SQLDatabase
 from langchain.chains.sql_database.query import create_sql_query_chain
 from langchain.chat_models import ChatOpenAI
-from langchain.utilities import SQLDatabase
+from langchain_community.utilities import SQLDatabase
+#from langchain.sql_database import SQLDatabase
 import pandas as pd
+import ast
 
 # Load environment variables from .env file (Optional)
 load_dotenv()
 
 #set up some local variables
 OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
-DB_ABS_PATH=os.getenv("DB_ABS_PATH")
+#DB_ABS_PATH=os.getenv("DB_ABS_PATH")
+#DB_REL_PATH = "dbchinook/Chinook.db"
 
-db = SQLDatabase.from_uri("sqlite:///{DB_ABS_PATH}")
+db = SQLDatabase.from_uri("sqlite:///dbchinook/Chinook.db")
 chain = create_sql_query_chain(llm=ChatOpenAI(temperature=0,model="gpt-3.5-turbo-0613"), db=db)
 
 def main():
@@ -22,15 +25,18 @@ def main():
     st.title("🦜🔗 Talk to a SQL DB using LLMs")
     st.image('assets/sqliteERD.png')
     st.subheader('This example returns BOTH the SQL AND its execution.')
+    st.subheader('this version has the test SQL')
     question = st.text_input("Ask a question (query/prompt)")
     if st.button("Submit Query", type="primary"):
-
         sql_query = chain.invoke({"question": question})
         st.subheader("This is the SQL you asked for:")
         st.write(sql_query)
         st.subheader("This is the Data from running that SQL:")
         result = db.run(sql_query)
-        df = pd.DataFrame([dict(row) for row in result])
+
+        # convert result to table
+        data = ast.literal_eval(result) # convert str to list
+        df = pd.DataFrame(data)
         st.dataframe(df)
 
 if __name__ == '__main__':
